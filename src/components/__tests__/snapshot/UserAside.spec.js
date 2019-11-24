5ca8937cf53a6a7a7e471efd8fd7e0010929d566
Batian <bmuthoga@gmail.com>
2019-11-24 20:06:08 +0300

    add UserAside component snapshot tests

---
+++ src/components/__tests__/snapshot/UserAside.spec.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import toJson from 'enzyme-to-json'
+import { shallow } from 'enzyme'
+import UserAside from '../../UserAside'
+
+const props = {
+  profileImageUrl: 'http://lorempixel.com/200/200/',
+  username: 'testemail'
+}
+
+describe('UserAside Snapshot Tests', () => {
+  it('should render the UserAside component', () => {
+    const wrapper = shallow(
+      <UserAside { ...props } />
+    )
+
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+})
