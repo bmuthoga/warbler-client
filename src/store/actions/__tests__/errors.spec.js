08cd078d6837b8aa444b0ee0ca49a553ea34dca0
Batian <bmuthoga@gmail.com>
2019-11-26 23:59:41 +0300

    add errors actionCreators snapshot tests

---
+++ src/store/actions/__tests__/errors.spec.js
@@ -0,0 +1,21 @@
+import configureMockStore from 'redux-mock-store'
+import { addError, removeError } from '../errors'
+
+describe('Errors Snapshot Tests', () => {
+  let store
+  const mockStore = configureMockStore()
+
+  beforeEach(() => {
+    store = mockStore({})
+  })
+
+  it('should create ADD_ERROR action if adding an error is successful', () => {
+    store.dispatch(addError('Sample error message'))
+    expect(store.getActions()).toMatchSnapshot()
+  })
+
+  it('should create REMOVE_ERROR action if removing an error is successful', () => {
+    store.dispatch(removeError())
+    expect(store.getActions()).toMatchSnapshot()
+  })  
+})
