7d2d6a34ad757a91d00bf93c2d96a2f7d923c1c7
Batian <bmuthoga@gmail.com>
2019-11-27 00:31:15 +0300

    add loadMessages() and remove() actionCreators snapshot tests

---
+++ src/store/actions/__tests__/snapshot/messages.spec.js
@@ -0,0 +1,52 @@
+import configureMockStore from 'redux-mock-store'
+import {
+  loadMessages,
+  remove,
+  removeMessage,
+  fetchMessages,
+  postNewMessage
+} from '../../messages'
+
+describe('Messages Action Creators Snapshot Tests', () => {
+  let store
+  const mockStore = configureMockStore()
+
+  beforeEach(() => {
+    store = mockStore({})
+  })
+
+  it('should create LOAD_MESSAGES action if loading messages is successful', () => {
+    const messages = [
+      {
+        _id: '8995ijg85ug8u58g8g858',
+        text: 'First sample message',
+        user: {
+          _id: '7f99fd9r7f800g8g8g9g',
+          username: 'firstTestUser',
+          profileImageUrl: 'http://lorempixel.com/200/200/'
+        },
+        createdAt: '2019-11-23T16:45:46.174Z',
+        updatedAt: '2019-11-23T16:45:46.174Z'
+      },
+      {
+        _id: '3h5k5h45k6g6g6k6kj6h6k',
+        text: 'Second sample message',
+        user: {
+          _id: '4vb5kl97dsfu485gjig',
+          username: 'secondTestUser',
+          profileImageUrl: 'http://lorempixel.com/200/200/'
+        },
+        createdAt: '2019-11-23T16:45:46.174Z',
+        updatedAt: '2019-11-23T16:45:46.174Z'
+      }
+    ]
+    store.dispatch(loadMessages(messages))
+    expect(store.getActions()).toMatchSnapshot()
+  })
+
+  it('should create REMOVE_MESSAGE action if removing a message is successful', () => {
+    const messageId = '484hjf8r99g94jg9jg'
+    store.dispatch(remove(messageId))
+    expect(store.getActions()).toMatchSnapshot()
+  })
+})
