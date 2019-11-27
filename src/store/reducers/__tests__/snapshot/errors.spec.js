f5af32ec0d121968ad8f74aa549b2e17f888819d
Batian <bmuthoga@gmail.com>
2019-11-27 16:35:09 +0300

    add error reducer snapshot tests

---
+++ src/store/reducers/__tests__/snapshot/errors.spec.js
@@ -0,0 +1,17 @@
+import errorReducer from '../../errors'
+import { ADD_ERROR, REMOVE_ERROR } from '../../../actionTypes'
+
+describe('Error Reducer Sanpshot Tests', () => {
+  it('should return the initial state', () => {
+    expect(errorReducer(undefined, {})).toMatchSnapshot()
+  })
+
+  it('should handle ADD_ERROR', () => {
+    const error = 'Sample error message'
+    expect(errorReducer(undefined, { type: ADD_ERROR, error })).toMatchSnapshot()
+  })
+
+  it('should handle REMOVE_ERROR', () => {
+    expect(errorReducer(undefined, { type: REMOVE_ERROR })).toMatchSnapshot()
+  })
+})
